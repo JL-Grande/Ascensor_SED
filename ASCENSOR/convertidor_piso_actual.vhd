e04f62945a631c40cdeb222bc2cb274013d3240f
davidgrandegil <davidgrande30@gmail.com>
2016-12-28 14:18:59 +0100

    4 a 3 bits

--- ASCENSOR/convertidor_piso_actual.vhd
+++ ASCENSOR/convertidor_piso_actual.vhd
@@ -9,8 +9,8 @@ entity convertidor_piso_actual is
 		piso_actual: IN std_logic_vector(3 DOWNTO 0);
 		boton_seleccionado: IN std_logic_vector(3 DOWNTO 0);
 		
-		piso_actual_convertido: OUT std_logic_vector(1 DOWNTO 0);
-		boton_seleccionado_convertido: OUT std_logic_vector(1 DOWNTO 0)
+		piso_actual_convertido: OUT std_logic_vector(2 DOWNTO 0);
+		boton_seleccionado_convertido: OUT std_logic_vector(2 DOWNTO 0)
 		);
 end convertidor_piso_actual;
 
@@ -18,16 +18,16 @@ architecture dataflow of convertidor_piso_actual is
 
 begin
 	WITH piso_actual SELECT
-				piso_actual_convertido <=  "00" WHEN "0001",
-													"01" WHEN "0010",
-													"10" WHEN "0100",
-													"11" WHEN "1000",
-													"00" WHEN others;
+				piso_actual_convertido <=  "001" WHEN "0001",
+													"010" WHEN "0010",
+													"011" WHEN "0100",
+													"100" WHEN "1000",
+													"000" WHEN others;
 	WITH boton_seleccionado SELECT
-				boton_seleccionado_convertido <= "00" WHEN "0001",
-															"01" WHEN "0010",
-															"10" WHEN "0100",
-															"11" WHEN "1000",
-															"00" WHEN others;
+				boton_seleccionado_convertido <= "001" WHEN "0001",
+															"010" WHEN "0010",
+															"011" WHEN "0100",
+															"100" WHEN "1000",
+															"000" WHEN others;
 end dataflow;
 
