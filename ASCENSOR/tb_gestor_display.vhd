51882cd3f50713e1403c8fcdd3f9e58bd98649c1
jlGrande <joseluis.grande95@gmail.com>
2017-01-16 03:05:40 +0100

    Upload testbench

--- ASCENSOR/tb_gestor_display.vhd
+++ ASCENSOR/tb_gestor_display.vhd
@@ -1,3 +1,6 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
@@ -11,9 +14,9 @@ ARCHITECTURE behavior OF tb_gestor_display IS
     COMPONENT gestor_display
     PORT(
          CLK : IN  std_logic;
-         piso_now_3 : IN  std_logic_vector(2 downto 0);
-         piso_obj_3 : IN  std_logic_vector(2 downto 0);
-         piso_seleccionado : OUT  std_logic_vector(2 downto 0);
+         piso_now : IN  std_logic_vector(1 downto 0);
+         piso_obj : IN  std_logic_vector(1 downto 0);
+         piso_seleccionado : OUT  std_logic_vector(1 downto 0);
          piso_actual : OUT  std_logic_vector(1 downto 0);
          accion : OUT  std_logic_vector(1 downto 0)
         );
@@ -22,11 +25,11 @@ ARCHITECTURE behavior OF tb_gestor_display IS
 
    --Inputs
    signal CLK : std_logic := '0';
-   signal piso_now_3 : std_logic_vector(2 downto 0) := (others => '0');
-   signal piso_obj_3 : std_logic_vector(2 downto 0) := (others => '0');
+   signal piso_now : std_logic_vector(1 downto 0) := (others => '0');
+   signal piso_obj : std_logic_vector(1 downto 0) := (others => '0');
 
  	--Outputs
-   signal piso_seleccionado : std_logic_vector(2 downto 0);
+   signal piso_seleccionado : std_logic_vector(1 downto 0);
    signal piso_actual : std_logic_vector(1 downto 0);
    signal accion : std_logic_vector(1 downto 0);
 
@@ -38,8 +41,8 @@ BEGIN
 	-- Instantiate the Unit Under Test (UUT)
    uut: gestor_display PORT MAP (
           CLK => CLK,
-          piso_now_3 => piso_now_3,
-          piso_obj_3 => piso_obj_3,
+          piso_now => piso_now,
+          piso_obj => piso_obj,
           piso_seleccionado => piso_seleccionado,
           piso_actual => piso_actual,
           accion => accion
@@ -58,30 +61,28 @@ BEGIN
    -- Stimulus process
    stim_proc: process
    begin		
-	
       WAIT FOR 2 ns;
-		piso_now_3 <= "001";
-		piso_obj_3 <= "011";
+		piso_now <= "01";
+		piso_obj <= "11";
 		WAIT FOR 20 ns;
-		piso_now_3 <= "010";
+		piso_now <= "10";
 		WAIT FOR 20 ns;
-		piso_now_3 <= "011";
+		piso_now <= "11";
 		WAIT FOR 20 ns;
-		piso_obj_3 <= "100";
+		piso_obj <= "00";
 		WAIT FOR 20 ns;
-		piso_now_3 <= "100";
+		piso_now <= "10";
 		WAIT FOR 20 ns;
-		piso_obj_3 <= "010";
+		piso_obj <= "01";
 		WAIT FOR 20 ns;
-		piso_now_3 <= "011";
+		piso_now <= "11";
 		WAIT FOR 20 ns;
-		piso_now_3 <= "000";
+		piso_now <= "00";
 		WAIT FOR 20 ns;
 		
 		ASSERT false
 			REPORT "Simulación finalizada. Test superado."
 			SEVERITY FAILURE;
-			
    end process;
 
 END;
--- ASCENSOR/tb_motor_puerta.vhd
+++ ASCENSOR/tb_motor_puerta.vhd
@@ -1,6 +1,10 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-
+ 
 ENTITY tb_motor_puerta IS
 END tb_motor_puerta;
  
@@ -12,6 +16,7 @@ ARCHITECTURE behavior OF tb_motor_puerta IS
     PORT(
          CLK : IN  std_logic;
          RST : IN  std_logic;
+         nivel : IN  std_logic;
          celula : IN  std_logic;
          accionar_puerta : IN  std_logic;
          actuador_puerta : OUT  std_logic
@@ -22,6 +27,7 @@ ARCHITECTURE behavior OF tb_motor_puerta IS
    --Inputs
    signal CLK : std_logic := '0';
    signal RST : std_logic := '0';
+   signal nivel : std_logic := '0';
    signal celula : std_logic := '0';
    signal accionar_puerta : std_logic := '0';
 
@@ -37,6 +43,7 @@ BEGIN
    uut: motor_puerta PORT MAP (
           CLK => CLK,
           RST => RST,
+          nivel => nivel,
           celula => celula,
           accionar_puerta => accionar_puerta,
           actuador_puerta => actuador_puerta
@@ -55,7 +62,7 @@ BEGIN
    -- Stimulus process
    stim_proc: process
    begin		
-		
+	
 		RST <= '0';
       celula <= '0';
 		WAIT FOR 3 ns;
@@ -76,16 +83,20 @@ BEGIN
 		celula <= '1';
 		WAIT FOR 5 ns;
 		accionar_puerta <= '0';
-		WAIT FOR 20 ns;
+		WAIT FOR 10 ns;
+		nivel <= '1';
+		WAIT FOR 10 ns;
 		celula <= '0';
 		WAIT FOR 5 ns;
 		accionar_puerta <= '0';
-		WAIT FOR 20 ns;
+		WAIT FOR 5 ns;
+		nivel <= '0';
+		WAIT FOR 15 ns;
 		
 		ASSERT false
 			REPORT "Simulacion finalizada. Test superado."
 			SEVERITY FAILURE;
-			
+	
    end process;
 
 END;
